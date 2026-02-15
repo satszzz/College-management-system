0eeea312557111446cdd19ff92fe0b9c11d1348d
satszzz <satheeswaran129@gmail.com>
2026-02-15 16:59:16 +0530

    Fix Express 5 router error by using app.use() middleware for SPA fallback

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../dist')));
 
     // Handle React routing, return all requests to React app
-    app.get(/.*/, (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.join(__dirname, '../dist', 'index.html'));
     });
 }
